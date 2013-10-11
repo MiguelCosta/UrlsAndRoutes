fbd41cf9b27a06267a09734c55d4e67160746467
Miguel Costa <miguelpintodacosta@gmail.com>
2013-10-11 12:05:24 +0100

    Adicionados controllers, views e ajustes nas Routes

--- UrlsAndRoutes/App_Start/RouteConfig.cs
+++ UrlsAndRoutes/App_Start/RouteConfig.cs
@@ -8,13 +8,28 @@ using System.Web.Routing;
 namespace UrlsAndRoutes {
     public class RouteConfig {
         public static void RegisterRoutes(RouteCollection routes) {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("ShopSchema2", "Shop/OldAction",
+                new { controller = "Home", action = "Index" });
+
+            routes.MapRoute("ShopSchema", "Shop/{action}",
+                new { controller = "Home" });
+
+            routes.MapRoute("", "X{controller}/{action}");
+
+            routes.MapRoute("MyRoute", "{controller}/{action}",
+                new { controller = "Home", action = "Index" });
+
+            routes.MapRoute("", "REI/{controller}/{action}",
+                new { controller = "Home", action = "Index" });
+
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
